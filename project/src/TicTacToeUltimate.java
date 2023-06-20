d0b1128c9abdddc136b1a15423bfac8e621f4a00
crzjee <120662620+crzjee@users.noreply.github.com>
2023-06-20 21:51:32 +0200

    Update

--- project/src/TicTacToeUltimate.java
+++ project/src/TicTacToeUltimate.java
@@ -1,75 +1,10 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
-import javax.swing.*;
 
-
-
-public class TicTacToeUltimate implements ActionListener{
-
-    boolean playerX_turn;
-    Random random = new Random();
-
-    /** Plansza */
-    JFrame window = new JFrame("Ultimate Tic Tac Toe");
-    JPanel titlePanel = new JPanel();
-    JPanel buttonPanel = new JPanel();
-    JPanel optPanel = new JPanel();
-    JLabel textfield = new JLabel();
-    JButton[] buttons = new JButton[9];
-
-
-
-
-
-
-    TicTacToeUltimate() {
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(600, 600);
-        window.getContentPane().setBackground(new Color(250, 250, 0));
-        window.setLayout(new BorderLayout());
-        window.add(optPanel, BorderLayout.SOUTH);
-        window.setVisible(true);
-
-        textfield.setBackground(new Color(21, 27, 31));
-        textfield.setForeground(new Color(255, 255, 255));
-        textfield.setFont(new Font("Sans Serif", Font.BOLD, 30));
-        textfield.setHorizontalAlignment(JLabel.CENTER);
-        textfield.setText("Ultimate Tic Tac Toe");
-        textfield.setOpaque(true);
-
-
-        titlePanel.setLayout(new BorderLayout());
-        titlePanel.setBounds(0, 0, 600, 50);
-
-        buttonPanel.setLayout(new GridLayout(3, 3));
-        buttonPanel.setBackground(new Color(0, 245, 0));
-
-        JPanel mainPanel = new JPanel();
-        mainPanel.setBackground(new Color(21,27,31));
-        mainPanel.setLayout(new GridLayout(3,3,10,10));
-        for(int i=0;i< 9;i++){
-            window.add(mainPanel, BorderLayout.CENTER);
-            for( i=0; i<9; i++){
-                buttons[i] = new JButton();
-                mainPanel.add(buttons[i]);
-                buttons[i].setBackground(new Color(0,0,0));
-                buttons[i].setFont(new Font("Sans Serif", Font.BOLD, 30));
-                buttons[i].setFocusable(false);
-                buttons[i].addActionListener(this);
-            }
-        }
-
-        titlePanel.add(textfield);
-        window.add(titlePanel, BorderLayout.NORTH);
-
-
-        firstTurn();
-    }
-
-
+public class TicTacToeUltimate extends gameWindow{
     @Override
     public void actionPerformed(ActionEvent e) {
+
         for (int i = 0; i < 9; i++) {
             if (e.getSource() == buttons[i]) {
                 if (playerX_turn) {
@@ -93,22 +28,6 @@ public class TicTacToeUltimate implements ActionListener{
         }
     }
 
-        public void firstTurn () {
-
-            try {
-                Thread.sleep(1234);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            if (random.nextInt(2) == 0) {
-                playerX_turn = true;
-                textfield.setText("Ruch krzyżyka");
-            } else {
-                playerX_turn = false;
-                textfield.setText("Ruch kółka");
-            }
-        }
 
         public void check () {
             /* X win condition */
--- project/src/gameWindow.java
+++ project/src/gameWindow.java
@@ -1,7 +1,78 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class gameWindow{
+public abstract class gameWindow implements ActionListener{
+    boolean playerX_turn;
+    Random random = new Random();
 
+    /** Plansza */
+    JFrame window = new JFrame("Ultimate Tic Tac Toe");
+    JPanel titlePanel = new JPanel();
+    JPanel buttonPanel = new JPanel();
+    JPanel optPanel = new JPanel();
+    JLabel textfield = new JLabel();
+    JButton[] buttons = new JButton[9];
+
+
+    gameWindow() {
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(600, 600);
+        window.getContentPane().setBackground(new Color(250, 250, 0));
+        window.setLayout(new BorderLayout());
+        window.add(optPanel, BorderLayout.SOUTH);
+        window.setVisible(true);
+
+        textfield.setBackground(new Color(21, 27, 31));
+        textfield.setForeground(new Color(255, 255, 255));
+        textfield.setFont(new Font("Sans Serif", Font.BOLD, 30));
+        textfield.setHorizontalAlignment(JLabel.CENTER);
+        textfield.setText("Ultimate Tic Tac Toe");
+        textfield.setOpaque(true);
+
+
+        titlePanel.setLayout(new BorderLayout());
+        titlePanel.setBounds(0, 0, 600, 50);
+
+        buttonPanel.setLayout(new GridLayout(3, 3));
+        buttonPanel.setBackground(new Color(0, 245, 0));
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBackground(new Color(21,27,31));
+        mainPanel.setLayout(new GridLayout(3,3,10,10));
+        for(int i=0;i< 9;i++){
+            window.add(mainPanel, BorderLayout.CENTER);
+            for( i=0; i<9; i++){
+                buttons[i] = new JButton();
+                mainPanel.add(buttons[i]);
+                buttons[i].setBackground(new Color(0,0,0));
+                buttons[i].setFont(new Font("Sans Serif", Font.BOLD, 30));
+                buttons[i].setFocusable(false);
+                buttons[i].addActionListener(this);
+            }
+        }
+
+        titlePanel.add(textfield);
+        window.add(titlePanel, BorderLayout.NORTH);
+
+
+        firstTurn();
+    }
+    public void firstTurn () {
+
+        try {
+            Thread.sleep(1234);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if (random.nextInt(2) == 0) {
+            playerX_turn = true;
+            textfield.setText("Ruch krzyżyka");
+        } else {
+            playerX_turn = false;
+            textfield.setText("Ruch kółka");
+        }
+    }
 }
